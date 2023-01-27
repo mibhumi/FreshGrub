145958d992408c0a7a4c00d0f78cdf1fc88eebd2
Bhumi Patel <bhumipatel1196@gmail.com>
2023-01-27 17:47:39 -0500

    Add contact, about us and error page

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+        <h1>Something went wrong!</h1>
+        <p>Please try again later</p>
+    </div>
+  )
+}
+
+export default  Error;

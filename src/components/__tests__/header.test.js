c6e415bc0e785ea4e4071c4f6d73f7980c310470
Bhumi Patel <bhumipatel1196@gmail.com>
2023-02-16 19:12:04 -0500

    create useRestaurant hook

---
+++ src/components/__tests__/header.test.js
@@ -0,0 +1,11 @@
+import Header from "../Header";
+import { render } from "@testing-library/react";
+
+test("logo should load on rendering header", () => {
+
+    // load header
+    const header = render(<Header />);
+
+    // check if logo is loaded or not
+
+});

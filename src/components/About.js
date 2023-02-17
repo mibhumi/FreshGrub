a4b70745901024282cb92220aa7b0e2da9cdc3bb
Bhumi Patel <bhumipatel1196@gmail.com>
2023-02-16 22:32:12 -0500

    useconext consume implementation

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import UserContext from "../utils/UserContext";
 
 const About = () => {
   return (
     <div>
         <h1>About</h1>
+        <UserContext.Consumer>
+            {(user)=> (
+                <h4> {user.name} </h4>
+            )}
+        </UserContext.Consumer>
         <p>I am making online food ordering application</p>
     </div>
   )
